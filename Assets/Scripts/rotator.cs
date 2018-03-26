d8907970587cd7e058628e6a2175a913a99e8180
Josceline Feng <22095436+JoscelineF@users.noreply.github.com>
2018-03-25 18:45:31 -0700

    Add files via upload

---
+++ Assets/Scripts/rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotator : MonoBehaviour {
+
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (45, 30, 15) * Time.deltaTime);
+	}
+}
